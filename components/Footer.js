6e00addcfada8b230109955cb82c32a9ac9f0e20
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2022-12-22 14:51:20 +0530

    home ui

--- components/Footer.js
+++ components/Footer.js
@@ -1,12 +1,92 @@
 import React from 'react'
+import {TfiYoutube} from 'react-icons/tfi'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {FaFacebookF,FaLinkedinIn} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div className='h-[36vw] bg-[#efefef] pt-[3.2vw] w-full'>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
+    <div className='h-[36vw] bg-[#efefef] text-[1.25vw] pt-[3.2vw] w-full flex flex-col justify-between'>
+    <div className='flex px-[5vw]'>
+      <div className='flex flex-col w-1/4 px-[1.2vw]'>
+        <div className='flex flex-col my-[1vw] gap-[0.2vw]'>
+          <h1 className='text-orange-400 text-[1.1vw] font-bold'>About</h1>
+          <span>Company Profile</span>
+          <span>Manufacturing Facilities</span>
+          <span>Group Companies</span>
+        </div>
+        <div className='flex flex-col my-[1vw] gap-[0.2vw]'>
+          <h1 className='text-orange-400 text-[1.1vw] font-bold'>Products</h1>
+          <span>Inverter Batteries</span>
+          <span>Solar Batteries</span>
+          <span>EV Lithium Ion Batteries</span>
+          <span>E-Rickshaw Batteries</span>
+          <span>Nasaka</span>
+          <span>Lithium</span>
+        </div>
+      </div>
+      <div className='flex flex-col w-1/4 px-[1.2vw]'>
+        <div className='flex flex-col my-[1vw] gap-[0.2vw]'>
+          <h1 className='text-orange-400 text-[1.1vw] font-bold'>Urja Care</h1>
+          <span>Book a complaint</span>
+          <span>Escalate Your Request</span>
+          <span>Product Registration</span>
+          <span>Service Request Status</span>
+          <span>Battery AMC</span>
+          <span>Warranty Status</span>
+        </div>
+        <div className='flex flex-col my-[1vw] gap-[0.2vw]'>
+          <h1 className='text-orange-400 text-[1.1vw] font-bold'>Expertise</h1>
+          <span>Technology</span>
+          <span>Testing</span>
+          <span>R & D</span>
+          <span>Quality Control</span>
+          <span>Applications</span>
+        </div>
+      </div>
+      <div className='flex flex-col w-1/4 px-[1.2vw]'>
+        <div className='flex flex-col my-[1vw] gap-[0.2vw]'>
+          <h1 className='text-orange-400 text-[1.1vw] font-bold'>Media</h1>
+          <span>Press Releases</span>
+          <span>Events & Exhibitions</span>
+          <span>Awards & recognition</span>
+          <span>Media Coverage</span>
+          <span>TV Commercials</span>
+        </div>
+        <div className='flex flex-col my-[1vw] gap-[0.2vw]'>
+          <h1 className='text-orange-400 text-[1.1vw] font-bold'>Blog</h1>
+          <span>Contact Us</span>
+          <span>Store Locator</span>
+          <span>Post a Query</span>
+          <span>Okaya Intra Site</span>
+        </div>
+      </div>
+      <div className='flex flex-col w-1/4 gap-[1vw] text-[1.1vw] px-[1.2vw]'>
+        <div className='text-orange-400 font-bold'>
+          <span className=''>Follow Us</span>
+          <div className='flex mt-[1vw] gap-[2vw]'>
+          <AiOutlineTwitter/>
+          <FaLinkedinIn/>
+          <FaFacebookF/>
+          <TfiYoutube/>
+          </div>
+        </div>
+        <div>
+          <span className='text-orange-400 font-bold '>Subscribe to our Newsletter</span>
+          <div className='mt-[1vw]'>
+            <input placeholder='enter your email' className='bg-white p-[0.4vw]'/>
+            <button className='bg-orange-400 text-white ml-[0.8vw] drop-shadow-md px-[1vw] py-[0.5vw]'>Submit</button>
+          </div>
+        </div>
+        <div>
+          <div className='text-orange-400 font-bold'>Looking to buy a battery ?</div>
+          <button>Quick Buy</button>
+        </div>
+      </div>
+    </div>
+
+    <div className='flex justify-center bg-gray-300 text-[#404041] py-[0.4vw] items-center'>
+      All right reserved @ Urja Kendra pvt. ltd. 2022
+    </div>
     </div>
   )
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -4,10 +4,10 @@ import { FcAndroidOs } from 'react-icons/fc'
 
 const Navbar = () => {
   return (
-    <div className='bg-white flex h-[9vw] text-[1.25vw] items-center px-[2vw] text-[#333333] w-full'>
+    <div className='bg-white flex h-[9vw] text-[1.1vw] items-center px-[2vw] text-[#333333] w-full'>
 
       <div className='flex absolute gap-[1vw] text-[1vw] top-[0.5vw] items-center right-[1vw]'>
-        <span className='text-white bg-[#51aa5b] p-[0.5vw] rounded-md font-medium'>Why Should You Download Urja Care App</span>
+        <span className='text-white bg-orange-400 p-[0.5vw] rounded-md font-medium'>Why Should You Download Urja Care App</span>
         Download the App <FcAndroidOs className='text-[3vw]' />
       </div>
 
@@ -20,13 +20,13 @@ const Navbar = () => {
       {/* navbar */}
 
       <div className='flex w-5/6 pt-8 justify-between px-[2vw]'>
-        <div>About</div>
-        <div>Products & Solutions</div>
-        <div>Urja Care</div>
-        <div>Expertise</div>
-        <div>Export</div>
-        <div>Media</div>
-        <div>Contact</div>
+        <div className='hover:text-orange-400 transition-all duration-300 cursor-pointer'>About</div>
+        <div className='hover:text-orange-400 transition-all duration-300 cursor-pointer'>Products & Solutions</div>
+        <div className='hover:text-orange-400 transition-all duration-300 cursor-pointer'>Urja Care</div>
+        <div className='hover:text-orange-400 transition-all duration-300 cursor-pointer'>Expertise</div>
+        <div className='hover:text-orange-400 transition-all duration-300 cursor-pointer'>Export</div>
+        <div className='hover:text-orange-400 transition-all duration-300 cursor-pointer'>Media</div>
+        <div className='hover:text-orange-400 transition-all duration-300 cursor-pointer'>Contact</div>
       </div>
     </div>
   )
