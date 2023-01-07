60e709bce1f10f99946e788b47e31cde2c715540
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2023-01-07 11:24:24 +0530

    c

--- pages/Announcement.js
+++ pages/Announcement.js
@@ -1,13 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { useState, useEffect } from 'react'
-import Investor from '../components/investor';
+import React, { Suspense } from 'react'
+const Investor = React.lazy(()=>import('../components/investor'));
 import fs from 'fs'
 import path from 'path';
 const Announcement = ({pdfs}) => {
     return (
         <div className='mt-[7vw]'>
             <div className='h-[8vw] overflow-hidden'><img src='/1.png' alt='top-banner'/></div>
+            <Suspense fallback={<div>Loading...</div>}>
             <Investor  pdfs={pdfs} folder={"announcements"} />
+            </Suspense>
         </div>
     )
 }
