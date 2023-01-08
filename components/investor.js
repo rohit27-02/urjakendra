c657658af3bd923f5f83eb96cd710dcb9d356463
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2023-01-08 14:14:24 +0530

    c

--- components/investor.js
+++ components/investor.js
@@ -1,23 +1,30 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/jsx-no-target-blank */
 import React, { useState, useEffect } from 'react'
-import {FaFileDownload,FaEye} from "react-icons/fa"
+import { FaFileDownload, FaEye } from "react-icons/fa"
+import {MdArrowForwardIos,MdArrowBackIos} from "react-icons/md"
 
-const Investor = ({ pdfs ,folder}) => {
-  
+
+
+const Investor = ({ pdfs, folder, setstart, setend ,start,end}) => {
   return (
     <div style={{ fontFamily: "'Raleway', sans-serif" }} className=''>
-      
+
       <div className='py-[3vw] '>
-          <div className='h-[4vw] w-[88vw] text-[2vw] font-semibold pl-[4vw] py-[0.5vw] mx-auto bg-orange-500 text-white'>{folder}</div>
-        <div className='flex flex-wrap  w-[88vw] gap-[1vw] py-[2vw] shadow-lg mx-auto pl-[5vw]'>
-          {pdfs.map((pdf)=>{
-            return <div key='pdf' className='flex relative items-center justify-between p-[1vw] w-[30%]  bg-gray-100'>
-              <img className='h-[4vw]' src='/pdf_icon.png' alt='icon'/>
+        <div className='h-[4vw] w-[88vw] text-[2vw] font-semibold pl-[4vw] py-[0.5vw] mx-auto bg-orange-500 text-white'>{folder}</div>
+        
+        <div className='flex flex-wrap  w-[88vw] gap-[1vw] py-[2vw] shadow-lg mx-auto pl-[5vw] pt-[2vw] relative'>
+          <div className='absolute flex'>
+          <div className='z-50 cursor-pointer text-3xl  text-black' onClick={() => { setend(end - 25); setstart(start - 25) }}><MdArrowBackIos/></div>
+        <div className='z-50 cursor-pointer text-3xl  text-black' onClick={() => { setend(end + 25); setstart(start + 25) }}><MdArrowForwardIos/></div>
+          </div>
+          {pdfs.map((pdf) => {
+            return <div key={pdf} className='flex relative items-center justify-between p-[1vw] w-[30%]  bg-gray-100'>
+              <img className='h-[4vw]' src='/pdf_icon.png' alt='icon' />
               <h1 className='ml-[1vw] capitalize mr-[3vw] w-1/2 overflow-hidden'>{pdf}</h1>
               <div className=''>
-                <a  href={"/"+folder+"/"+pdf}><FaEye className='bg-orange-500 text-white h-[2.5vw] w-auto p-[0.5vw] mb-[1vw]'/></a>
-                <a  href={"/"+folder+"/"+pdf} download><FaFileDownload className='bg-green-500 text-white h-[2.5vw] w-auto p-[0.5vw] mt-[1vw]'/></a>
+                <a href={"/" + folder + "/" + pdf}><FaEye className='bg-orange-500 text-white h-[2.5vw] w-auto p-[0.5vw] mb-[1vw]' /></a>
+                <a href={"/" + folder + "/" + pdf} download><FaFileDownload className='bg-green-500 text-white h-[2.5vw] w-auto p-[0.5vw] mt-[1vw]' /></a>
               </div>
             </div>
           })}
--- pages/Announcement.js
+++ pages/Announcement.js
@@ -1,44 +1,33 @@
 /* eslint-disable @next/next/no-img-element */
-import React, { Suspense } from 'react'
-const Investor = React.lazy(()=>import('../components/investor'));
+import React, { Suspense, useState } from 'react'
+const Investor = React.lazy(() => import('../components/investor'));
 import fs from 'fs'
 import path from 'path';
-import axios from 'axios';
 
-const Announcement = () => {
-    const fetchFolder = async () => {
-        try {
-            const response = await axios.get('https://mega.nz/folder/OFs0xLbQ#y186JttT3_Xd72QjKexbbQ', { responseType: 'arraybuffer' });
-            // Save the PDF files to the server
-            // fs.writeFileSync('files.zip', response.data);
-            // Extract the PDF files from the zip file
-            // const unzipped = await unzip(response.data);
-            // Display the PDF files
-            console.log(response.data)
-          } catch (error) {
-            console.error(error);
-          }
-      };
-      fetchFolder();
-    return (
-        <div className='mt-[7vw]'>
-            <div className='h-[8vw] overflow-hidden'><img src='/1.png' alt='top-banner'/></div>
-            <Suspense fallback={<div>Loading...</div>}>
-            {/* <Investor  pdfs={pdfs} folder={"announcements"} /> */}
-            </Suspense>
-        </div>
-    )
+
+
+const Announcement = ({ pdfs }) => {
+  const [start, setstart] = useState(0);
+  const [end, setend] = useState(24);
+  return (
+    <div className='mt-[7vw]'>
+      <div className='h-[8vw] overflow-hidden'><img src='/1.png' alt='top-banner' /></div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Investor pdfs={pdfs.slice(start, end)} setstart={setstart} setend={setend} start={start} end={end} folder={"announcements"} />
+      </Suspense>
+    </div>
+  )
 }
 
 export default Announcement
 
-// export async function getServerSideProps() {
-//     // Fetch data from external API
-//     let res=[] ;
-//     for (const file of fs.readdirSync(path.resolve("https://mega.nz/folder/OFs0xLbQ#y186JttT3_Xd72QjKexbbQ"))) {
-//         res=[...res,file];
-  
-//     }
-  
-//     return { props: {pdfs:res} }
-//   }
+export async function getServerSideProps() {
+  // Fetch data from external API
+  let res = [];
+  for (const file of fs.readdirSync(path.resolve("\public/announcements"))) {
+    res = [...res, file];
+
+  }
+
+  return { props: { pdfs: res } }
+}
