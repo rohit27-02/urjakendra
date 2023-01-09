28d15ef5b8dedd20b48ad1a121fbb1df15de2e9e
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2023-01-09 13:33:05 +0530

    c

--- next.config.js
+++ next.config.js
@@ -2,11 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: { 
+    nftTracing: true 
+  }
 }
 
 module.exports = nextConfig
 module.exports = {
-  webpack5: true,
+  webpack5: false,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
 
