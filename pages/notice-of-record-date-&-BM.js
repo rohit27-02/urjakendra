e2f058ccda55c5b1ccb6c62b23273d3213e59ed8
rohit rawat <102246254+rohit27-02@users.noreply.github.com>
2023-03-04 13:36:23 +0530

    c

--- pages/notice-of-record-date-&-BM.js
+++ pages/notice-of-record-date-&-BM.js
@@ -3,11 +3,11 @@ import React, { useState, useEffect } from 'react'
 import Investor from '../components/investor';
 import fs from 'fs'
 import path from 'path';
-const Notice = ({pdfs}) => {
+const Notice = ({ pdfs }) => {
     return (
         <div className='mt-[8vh] md:mt-[7vw]'>
-            <div className='h-[8vh] md:h-[8vw] overflow-hidden'><img src='/1.png' alt='top-banner'/></div>
-            <Investor  pdfs={pdfs} folder={"notice of record date & BM"} />
+            <div className='h-[8vh] md:h-[8vw] overflow-hidden'><img src='/1.png' alt='top-banner' /></div>
+            <Investor pdfs={pdfs} folder={"notice of record date & BM"} />
         </div>
     )
 }
@@ -16,11 +16,24 @@ export default Notice
 
 export async function getServerSideProps() {
     // Fetch data from external API
-    let res=[] ;
-    for (const file of fs.readdirSync(path.resolve("\public/notice of record date & BM"))) {
-        res=[...res,file];
-  
+    let res = [];
+   
+
+    for (const file of  fs.readdirSync(path.resolve("\public/notice of record date & BM"))) {
+        res = [...res, file]
     }
-  
-    return { props: {pdfs:res} }
-  }
+    res=res.map(function (fileName) {
+        return {
+            name: fileName,
+            time: fs.statSync(path.resolve("\public/notice of record date & BM") + '/' + fileName).mtime.getTime()
+        };
+    })
+        .sort(function (a, b) {
+            return b.time - a.time;
+        })
+        .map(function (v) {
+            return v.name;
+        });
+
+    return { props: { pdfs: res } }
+}
